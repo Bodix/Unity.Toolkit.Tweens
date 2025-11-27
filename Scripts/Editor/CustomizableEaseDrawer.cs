d2b27c5b382b419d2e5dfdadc7bf91b10b015acc
Bodix <bodix321@gmail.com>
2025-11-28 01:05:43 +0200

    Refactor `CustomizableEase` property drawer

--- Scripts/Editor/CustomizableEaseDrawer.cs
+++ Scripts/Editor/CustomizableEaseDrawer.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright © 2025 Bogdan Nikolayev <bodix321@gmail.com>
 // All Rights Reserved
 
-using DG.Tweening;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,63 +9,27 @@ namespace Toolkit.Tweens.Editor
 	[CustomPropertyDrawer(typeof(CustomizableEase))]
 	public class CustomizableEaseDrawer : PropertyDrawer
 	{
-		private const float TOGGLE_WIDTH = 80f;
-		private const float SPACING = 4f;
+		private const float ToggleWidth = 70f;
+		private const float Spacing = 4f;
 
 		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
 		{
-			var isCustomProp = property.FindPropertyRelative(nameof(CustomizableEase.IsCustom));
-			float height = EditorGUIUtility.singleLineHeight;
-
-			if (isCustomProp.boolValue)
-			{
-				var curveProp = property.FindPropertyRelative(nameof(CustomizableEase.CustomCurve));
-				height += EditorGUI.GetPropertyHeight(curveProp, true);
-			}
-
-			return height;
+			return EditorGUIUtility.singleLineHeight;
 		}
 
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 		{
 			position = EditorGUI.PrefixLabel(position, label);
 
-			// Save current indent, to not break indentation.
-			int oldIndent = EditorGUI.indentLevel;
-			EditorGUI.indentLevel = 0;
+			Rect toggleRect = new Rect(position.x, position.y, ToggleWidth, EditorGUIUtility.singleLineHeight);
+			Rect propertyRect = new Rect(position.x + ToggleWidth + Spacing, position.y,
+				position.width - ToggleWidth - Spacing, EditorGUIUtility.singleLineHeight);
+			SerializedProperty isCustomProperty = property.FindPropertyRelative(nameof(CustomizableEase.IsCustom));
+			SerializedProperty easeProperty = property.FindPropertyRelative(nameof(CustomizableEase.Ease));
+			SerializedProperty customCurveProperty = property.FindPropertyRelative(nameof(CustomizableEase.CustomCurve));
 
-			Rect toggleRect = new Rect(position.x, position.y, TOGGLE_WIDTH, EditorGUIUtility.singleLineHeight);
-			Rect easeRect = new Rect(position.x + TOGGLE_WIDTH + SPACING, position.y, position.width - TOGGLE_WIDTH - SPACING, EditorGUIUtility.singleLineHeight);
-
-			var isCustomProp = property.FindPropertyRelative(nameof(CustomizableEase.IsCustom));
-			var easeTypeProp = property.FindPropertyRelative(nameof(CustomizableEase.Ease));
-			var curveProp = property.FindPropertyRelative(nameof(CustomizableEase.CustomCurve));
-
-			bool newIsCustom = EditorGUI.ToggleLeft(toggleRect, "Custom", isCustomProp.boolValue);
-			if (newIsCustom != isCustomProp.boolValue)
-			{
-				isCustomProp.boolValue = newIsCustom;
-
-				if (newIsCustom)
-					curveProp.animationCurveValue = AnimationCurve.EaseInOut(0, 0, 1, 1);
-				else
-					easeTypeProp.enumValueIndex = (int)Ease.OutQuad;
-			}
-
-			if (isCustomProp.boolValue)
-			{
-				// Offset down for curve.
-				Rect curveRect = new Rect(position.x, position.y + EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing,
-					position.width, EditorGUI.GetPropertyHeight(curveProp, true));
-
-				EditorGUI.PropertyField(curveRect, curveProp, GUIContent.none);
-			}
-			else
-			{
-				EditorGUI.PropertyField(easeRect, easeTypeProp, GUIContent.none);
-			}
-
-			EditorGUI.indentLevel = oldIndent;
+			isCustomProperty.boolValue = EditorGUI.ToggleLeft(toggleRect, "Custom", isCustomProperty.boolValue);
+			EditorGUI.PropertyField(propertyRect, isCustomProperty.boolValue ? customCurveProperty : easeProperty, GUIContent.none);
 		}
 	}
 }
