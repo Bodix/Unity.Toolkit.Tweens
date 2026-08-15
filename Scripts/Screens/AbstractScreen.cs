35611d4c004350dfe232d81d121588a027f3e428
Bodix <bodix321@gmail.com>
2026-08-15 08:11:09 +0300

    Handle back button pressing for screen

--- Scripts/Screens/AbstractScreen.cs
+++ Scripts/Screens/AbstractScreen.cs
@@ -1,13 +1,21 @@
 ﻿// Copyright © 2025 Bogdan Nikolayev <bodix321@gmail.com>
 // All Rights Reserved
 
+using Bodix.Evolunity.Services;
 using DG.Tweening;
 using UnityEngine;
 
 namespace Toolkit.Tweens.Screens
 {
-	public abstract class AbstractScreen : MonoBehaviour
+	public abstract class AbstractScreen : MonoBehaviour, IBackHandler
 	{
+		protected IBackNavigationService BackNavigationService;
+
+		public virtual void Construct(IBackNavigationService backNavigationService)
+		{
+			BackNavigationService = backNavigationService;
+		}
+
 		public virtual bool IsEnabled
 		{
 			get => gameObject.activeSelf;
@@ -17,11 +25,26 @@ namespace Toolkit.Tweens.Screens
 		public virtual void Show()
 		{
 			IsEnabled = true;
+			BackNavigationService?.Register(this);
 		}
 
 		public virtual void Hide()
 		{
 			IsEnabled = false;
+			BackNavigationService?.Unregister(this);
+		}
+
+		/// <summary>
+		/// Pops the screen when back navigation is triggered.
+		/// </summary>
+		public virtual bool OnBackPressed()
+		{
+			if (!IsEnabled || ScreenStack.CurrentScreen != this)
+				return false;
+
+			Pop();
+
+			return true;
 		}
 
 		public virtual Tween Push()
